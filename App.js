0e7232566733c96e5f58f51ac3fcc0bd0a5495be
Kevin Moy MAC <kmoy2014@gmail.com>
2020-11-04 14:54:04 -0500

    Set up emulators on Mac and Android

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, SafeAreaView, View } from "react-native";
+import Login from "./components/Login"
 
 export default function App() {
+  console.log("App executed!");
+  console.log(Login)
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Login />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
+/**
+ * STYLESHEET
+ */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#85C1E9",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView, View } from "react-native";
+
+export default class Login extends React.Component{
+
+  render(){
+    return(
+      <View>
+        <Text>This is for the login</Text>
+      </View>
+    )
+  }
+}
