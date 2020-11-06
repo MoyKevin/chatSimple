857e065c0b2e7070e41c8bbddf58868affd1e558
Kevin Moy MAC <kmoy2014@gmail.com>
2020-11-06 14:01:33 -0500

    working on google oauth

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ export default function App() {
   console.log(Login)
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <Login />
       <StatusBar style="auto" />
     </SafeAreaView>
--- components/Login.js
+++ components/Login.js
@@ -1,13 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView, View } from "react-native";
+import { StyleSheet, Text, SafeAreaView, View, TextInput,Button } from "react-native";
 
 export default class Login extends React.Component{
 
   render(){
     return(
       <View>
-        <Text>This is for the login</Text>
+        <Text style={styles.titleText}>chatSimple</Text>
+        <View style={styles["login-form"]}>
+          <Text>UserName:</Text>
+          <TextInput
+            style={styles.inputField}
+          />
+          <Text>Password:</Text>
+          <TextInput
+            style={styles.inputField}
+          />
+          <Button style={styles.OauthButtons} title="Google Oauth"/>
+          <Text style={styles.OauthButtons}>Facebook Auth?</Text>
+        </View>
       </View>
     )
   }
 }
+
+/**
+ * STYLESHEET
+ */
+const styles = StyleSheet.create({
+  'login-form': {
+    width: 300,
+    height: 200,
+    backgroundColor: "#FFF",
+    justifyContent: "center",
+    padding: 20,
+    borderRadius: 15,
+    borderTopRightRadius: 50,
+    borderBottomLeftRadius: 50
+  },
+  titleText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#FFF'
+  },
+  inputField: {
+    height: 30,
+    borderWidth: 1,
+    borderColor: '#000',
+  },
+  OauthButtons: {
+    alignSelf: 'flex-end',
+  }
+});
